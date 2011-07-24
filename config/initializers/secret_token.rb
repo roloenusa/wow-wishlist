a24ad3940d0c5500ed4c2b3ee1ae2ab26fc76f2f
Juan Delgado <juan@grizzlypanda.com>
2011-07-23 23:34:57 -0700

    Init commit part deux

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WowWishlist::Application.config.secret_token = '4b02d8a48a3b5364cff317b41d57f9de2be0d4fec67a27f31c3d45f840b6fce5c5d4a99de20a81de9a100a4effd95e7e8f630e3d2ce94557995ec3698e189756'
